d01f4b2753de09a5d8f76a46e5cb9d8892328bdf
guna0223 <gunasekar0223@gmail.com>
2025-11-11 18:00:59 +0530

    added files

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,17 +1,39 @@
 import ChatListItem from "../ChatListItem/ChatListItem";
+import "../css/Sidebar.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 function Sidebar({ chats, onSelectChat, selectedChat }) {
   return (
-    <div className="sidebar">
-      {chats.map((chat) => (
-        <ChatListItem
-          key={chat.id}
-          chat={chat}
-          onClick={() => onSelectChat(chat)}
-          active={selectedChat?.id === chat.id}
-        />
-      ))}
-    </div>
+    <>
+
+      <div className="sidebar">
+        <div className="sidebar-section">
+          <h2 className="sidebar-title">
+            WhatApp
+          </h2>
+          <div>
+            <span className="chat-icon"><i class="bi bi-chat-square-text"></i></span>
+            <span className="menu-icon"><i class="bi bi-three-dots-vertical"></i></span>
+          </div>
+        </div>
+
+        <form action="">
+          <input type="text" placeholder="Search or start a newchat"/>
+        </form>
+
+        {chats.map((chat) => (
+          <ChatListItem
+            key={chat.id}
+            chat={chat}
+            onClick={() => onSelectChat(chat)}
+            active={selectedChat?.id === chat.id}
+          />
+        ))}
+      </div >
+    </>
   );
 }
 
